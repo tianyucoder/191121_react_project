aab97cb5c4ac176e28e1c955fae5aff3a988f69c
tianyu5177 <zhangtianyu5177@sina.com>
2020-04-27 17:06:28 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import {Button} from 'antd'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				App...
+				<Button type="primary">点我</Button>
 			</div>
 		)
 	}
