db8c2c5fa78ed35db93b0326ca61dfc6c52abe8b
tianyu5177 <zhangtianyu5177@sina.com>
2020-04-29 09:16:00 +0800

    redux分支的建立

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
-import Admin from './pages/Admin/Admin'
-import Login from './pages/Login/Login'
-import {Switch,Route,Redirect} from 'react-router-dom'
 
 export default class App extends Component {
 	render() {
 		return (
-			<Switch>
-				<Route path="/login" component={Login}/>
-				<Route path="/admin" component={Admin}/>
-				<Redirect to="login"/>
-			</Switch>
+			<div>
+				App....
+			</div>
 		)
 	}
 }
