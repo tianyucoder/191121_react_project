4b19e5b2a27041b84a6ebb4fdc3fba98adba841d
tianyu5177 <zhangtianyu5177@sina.com>
2020-04-30 15:58:43 +0800

    更新

---
+++ 10_src_纯函数_高阶函数/redux/store.js
@@ -0,0 +1,13 @@
+//该文件可以创建一个redux中最为核心的一个对象---store，随后会暴露出去。
+
+//引入createStore，用于创建store
+import {createStore,applyMiddleware} from 'redux' 
+//引入总reducer
+import allReducer from './reducers'
+//引入redux-thunk用于支持异步action
+import thunk from 'redux-thunk'
+//引入composeWithDevTools
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+//创建store，同时指定好为store所服务的reducer,随后暴露
+export default createStore(allReducer,composeWithDevTools(applyMiddleware(thunk)))
