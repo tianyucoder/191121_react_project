725669d2f9c85df6abcee5181cfbf80d3e993028
tianyu5177 <zhangtianyu5177@sina.com>
2020-05-04 10:05:36 +0800

    更新

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Admin extends Component {
 	render() {
 		return (
-			<div>
-				Admin...
+			<div style={{fontSize:'20px'}}>
+				欢迎，xxxx
 			</div>
 		)
 	}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,11 +10,12 @@ const {Item} = Form
 export default class Login extends Component {
 	//表单提交且验证通过的回调
 	onFinish = async values => {
-		let result = await reqLogin(values)
-		const {status,data,msg} = result
+		let result = await reqLogin(values) //获取请求结果
+		const {status,data,msg} = result //获取请求结果中的：status,data,msg
 		if(status === 0){
 			message.success('登录成功！',1)
 			console.log(data);
+			this.props.history.replace('/admin') //跳转页面
 		}else{
 			message.error(msg)
 		}
