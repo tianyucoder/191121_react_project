c6c7d3032fc395004ac313e56cebcd1d80779eaf
tianyu5177 <zhangtianyu5177@sina.com>
2020-05-06 15:01:09 +0800

    更新

--- src/containers/Admin/Category/Category.jsx
+++ src/containers/Admin/Category/Category.jsx
@@ -8,39 +8,40 @@ export default class Category extends Component {
 		const dataSource = [
 			{
 				key: '1',
-				name: '胡彦斌',
-				age: 32,
-				address: '西湖区湖底公园1号',
+				name: '测试分类00001',
 			},
 			{
 				key: '2',
-				name: '胡彦祖',
-				age: 42,
-				address: '西湖区湖底公园1号',
-			},
+				name: '测试分类00002',
+			}
 		];
-		//表格的列配置
+		//表格的列配置(特别重要)
 		const columns = [
 			{
-				title: '姓名',
-				dataIndex: 'name',
-				key: 'name',
+				title: '分类名', //列名
+				dataIndex: 'name', //数据索引项，控制该列展示什么信息。
+				key: '1', //不是一个必要的属性，和该列展示什么信息，没有任何关系，写上效率高
 			},
 			{
-				title: '年龄',
-				dataIndex: 'age',
-				key: 'age',
-			},
-			{
-				title: '住址',
-				dataIndex: 'address',
-				key: 'address',
+				title: '操作',
+				//dataIndex: 'name',
+				width:'20%',
+				align:'center',
+				render:() => <Button type="link">修改分类</Button>, //render用于高级渲染，返回值展示到页面
+				key: '3',
 			},
 		];
 
 		return (
 			<Card extra={<Button type="primary"><PlusCircleOutlined />添加</Button>}>
-				<Table dataSource={dataSource} columns={columns} />
+				<Table 
+					dataSource={dataSource} //配置数据源
+					columns={columns} //配置列
+					bordered //展示边框
+					pagination={{ //分页器
+						pageSize:4 //每页展示多少条
+					}} 
+				/>
 			</Card>
 		)
 	}
