569fe1f7de4e0bb151e565151a6b7189d2b1b118
tianyu5177 <zhangtianyu5177@sina.com>
2020-05-11 19:50:12 +0800

    更新

--- src/containers/Admin/Bar/Bar.jsx
+++ src/containers/Admin/Bar/Bar.jsx
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react';
 
 export default class Bar extends Component {
+
+	getOption = ()=>{
+		return {
+			title: {
+				text: '商品销售数据报表',
+				link:'http://www.atguigu.com/',
+				/* textStyle:{
+					color:'red'
+				} */
+			},
+			tooltip: {
+				show:true,
+				trigger:'axis',
+				showDelay:500
+			},
+			legend: {
+				data:['销量','库存']
+			},
+			xAxis: {
+				data: ["运动鞋","皮鞋","雪纺衫","裤子","高跟鞋","袜子"]
+			},
+			yAxis: {},
+			//series里的是数据
+			series: [
+				{
+					name: '销量',
+					type: 'bar', //以哪一种图表展示 bar柱状图 line折线图 pie饼状图
+					data: [5, 20, 36, 10, 10, 20]
+				},
+				{
+					name: '库存',
+					type: 'bar', //以哪一种图表展示 bar柱状图 line折线图 pie饼状图
+					data: [20, 25, 36, 40, 9, 50]
+				}
+			],
+			toolbox:{
+				 show:true,
+				 feature:{
+					 saveAsImage:{type:'gif'},
+					 restore:{},
+					 dataView:{}
+				 },
+				 
+			}
+		};
+	}
+
 	render() {
 		return (
 			<div>
-				Bar
+				<ReactEcharts option={this.getOption()}/>
 			</div>
 		)
 	}
